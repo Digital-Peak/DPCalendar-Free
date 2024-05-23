e166e6039c069adfbeaf694a2dec95448a0485cc
Allon Moritz <allon.moritz@digital-peak.com>
2024-05-23 06:18:22 +0000

    Version 9.0.0

--- com_dpcalendar/admin/sql/updates/mysql/9.0.0.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE `#__dpcalendar_events` CHANGE `catid` `catid` VARCHAR(255) NOT NULL DEFAULT '0';
-ALTER TABLE `#__dpcalendar_events` CHANGE `xreference` `xreference` VARCHAR(255) NULL;
-ALTER TABLE `#__dpcalendar_locations` CHANGE `xreference` `xreference` VARCHAR(255) NULL;
-ALTER TABLE `#__dpcalendar_bookings` CHANGE `uid` `uid` VARCHAR(255) NOT NULL DEFAULT '';
-ALTER TABLE `#__dpcalendar_tickets` CHANGE `uid` `uid` VARCHAR(255) NOT NULL DEFAULT '';
-ALTER TABLE `#__dpcalendar_extcalendars` CHANGE `plugin` `plugin` VARCHAR(255) NOT NULL DEFAULT '';

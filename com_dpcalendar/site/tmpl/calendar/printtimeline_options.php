99387026cc5f155d1a9fe1dbeda26b7e5da189aa
Allon Moritz <allon.moritz@digital-peak.com>
2024-05-23 08:52:48 +0000

    Version 9.0.0

--- com_dpcalendar/site/tmpl/calendar/printtimeline_options.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @package   DPCalendar
- * @copyright Copyright (C) 2021 Digital Peak GmbH. <https://www.digital-peak.com>
- * @license   https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL
- */
-defined('_JEXEC') or die();
-
-require __DIR__ . '/timeline_options.php';
